84a89cbdb60713e8996c2663afcfceb08977497e
wlwl0219 <wlwl0219@gmail.com>
2023-03-07 17:56:30 +0900

    react, styled-components props type

---
+++ src/Circle.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+interface CircleStyleProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+
+const Container = styled.div<CircleStyleProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 5px solid ${(props) => props.borderColor};
+`;
+
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
+}
+
+export default Circle;
