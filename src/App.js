3a07c05b78737a641190e49cbed259e2dfdcf661
wlwl0219 <wlwl0219@gmail.com>
2023-03-07 10:45:24 +0900

    styled-components animation, qseudo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
-`;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-const Circle = styled(Box)`
-  border-radius: 50px;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Input = styled.input.attrs({ required: true })`
+const rotationAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  50% {
+    border-radius:100px;
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius:0px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  ${Emoji}:hover {
+    font-size: 98px;
+  }
 `;
 
 function App() {
   return (
-    <Father as="header">
-      {/* <Box bgColor="teal" />
-      <Circle bgColor="tomato" /> */}
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <Emoji>🤩</Emoji>
+      </Box>
+      <Emoji>🔥</Emoji>
+    </Wrapper>
   );
 }
 
