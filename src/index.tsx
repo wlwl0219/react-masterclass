d7bcedb742b37c0ef0c94d24108de1ecc4c21c07
wlwl0219 <wlwl0219@gmail.com>
2023-03-10 17:42:10 +0900

    react-query coin list

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
 import { RouterProvider } from "react-router-dom";
 import { theme } from "./theme";
 import router from "./Router";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
