ffab1b65d55b053e60b7697db25b8f6b0bb74fcd
wlwl0219 <wlwl0219@gmail.com>
2023-03-15 00:01:07 +0900

    todoList recoil, react-hook-form set

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState  } from "../atoms";
+
+function ToDo({ text, category, id }: IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const {
+          currentTarget: { name },
+        } = event;
+      };
+
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
+  );
+}
+
+export default ToDo;
