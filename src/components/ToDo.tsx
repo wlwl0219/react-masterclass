e5218f956e4b0ef7a43a3196832076079f8f976f
wlwl0219 <wlwl0219@gmail.com>
2023-03-15 14:22:12 +0900

    category update

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { useSetRecoilState } from "recoil";
-import { IToDo, toDoState  } from "../atoms";
+import { IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
-    const setToDos = useSetRecoilState(toDoState);
+  const setToDos = useSetRecoilState(toDoState);
 
-    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        const {
-          currentTarget: { name },
-        } = event;
-      };
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+
+    setToDos((prevToDos) => {
+      return prevToDos.map((todo) =>
+        todo.id === id ? { ...todo, category: name as any } : todo
+      );
+    });
+  };
 
   return (
     <li>
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,7 +1,7 @@
 import { useRecoilValue } from "recoil";
 import { toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
-import ToDo from "./Todo";
+import ToDo from "./ToDo";
 
 function ToDoList() {
   const toDos = useRecoilValue(toDoState);
