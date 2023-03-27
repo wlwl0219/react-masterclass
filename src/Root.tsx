a8da36592bf1969824d2a45cf877920ff612e999
wlwl0219 <wlwl0219@gmail.com>
2023-03-27 17:04:38 +0900

    basic variants

--- src/Root.tsx
+++ src/Root.tsx
@@ -9,7 +9,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
@@ -17,12 +17,20 @@ const Box = styled.div`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 2 } },
+};
+
 function Root() {
   return (
     <Wrapper>
-      <Box />
-      <div></div>
-      <motion.div></motion.div>
+      {/* <Box
+        transition={{ type: "spring", delay: 2 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      /> */}
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -61,7 +61,6 @@ table {
 body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
-  background:linear-gradient(135deg,#e09,#d0e);
   color:black;
   line-height: 1.2;
 }
