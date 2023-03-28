4ff196a8435a543fb3d6ed32d6262a593a871afc
wlwl0219 <wlwl0219@gmail.com>
2023-03-28 14:25:09 +0900

    svg animate

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import Svg from "./SvgAni";
 import Root from "./Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
+    element: <Svg />,
   },
 ]);
 
